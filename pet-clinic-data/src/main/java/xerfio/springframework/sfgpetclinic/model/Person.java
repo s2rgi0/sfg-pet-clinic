12dd6f44c6bd5485254905065df25e283962aaf0
s2rgi0 <tlakolu@gmail.com>
2020-05-26 22:21:52 -0600

    Adding base entity. closes #10

--- pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package xerfio.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package xerfio.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
