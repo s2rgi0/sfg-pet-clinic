07fcf51376b82f39e98bccb90a1cf57d143f8595
s2rgi0 <tlakolu@gmail.com>
2020-10-03 17:32:22 -0600

    Added Owners index Page ,  closes #16

--- pet-clinic-web/src/main/java/xerfio/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/xerfio/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package xerfio.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import xerfio.springframework.sfgpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService  vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+
         return "vets/index";
     }
 
