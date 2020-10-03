56a112555099da8800c7fbf77cc058542200b8f8
s2rgi0 <tlakolu@gmail.com>
2020-10-03 17:40:38 -0600

    Added VetService to VetController and Vets index Page ,  closes #15

--- pet-clinic-web/src/main/java/xerfio/springframework/sfgpetclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/xerfio/springframework/sfgpetclinic/boostrap/DataLoader.java
@@ -30,6 +30,7 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+
         Owner owner1 = new Owner();
         owner1.setId(1L);
         owner1.setFirstName("Michael");
