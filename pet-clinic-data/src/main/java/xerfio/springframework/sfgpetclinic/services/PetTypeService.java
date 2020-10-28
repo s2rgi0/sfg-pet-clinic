ec4a59b3c3090e71e7ab3c01d9b5b5222af5dd92
s2rgi0 <tlakolu@gmail.com>
2020-10-28 07:50:26 -0600

    Added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package xerfio.springframework.sfgpetclinic.services;
+
+import xerfio.springframework.sfgpetclinic.CrudService;
+import xerfio.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType , Long> {
+
+
+}
