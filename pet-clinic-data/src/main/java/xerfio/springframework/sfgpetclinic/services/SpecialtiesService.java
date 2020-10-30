0d9467d01861206741dec4cb8adb6a855ccba327
s2rgi0 <tlakolu@gmail.com>
2020-10-29 18:09:28 -0600

    Added Specialties Map Service  closes #31

---
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package xerfio.springframework.sfgpetclinic.services;
+
+import xerfio.springframework.sfgpetclinic.CrudService;
+import xerfio.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/services/map/SpecialtiesMapService.java
@@ -0,0 +1,23 @@
+package xerfio.springframework.sfgpetclinic.services.map;
+
+import xerfio.springframework.sfgpetclinic.model.Specialty;
+import xerfio.springframework.sfgpetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialtiesMapService  extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() { return super.findAll(); }
+
+    @Override
+    public void deleteById(Long id) { super.deleteById(id); }
+
+    @Override
+    public void delete(Specialty object) { super.delete(object); }
+
+    @Override
+    public Specialty save(Specialty object) { return super.save(object); }
+
+    @Override
+    public Specialty findById(Long id) { return super.findById(id); }
+}
