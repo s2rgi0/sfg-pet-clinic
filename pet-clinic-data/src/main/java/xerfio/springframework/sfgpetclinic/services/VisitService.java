1a16b2e9edff6ee9c17a94517134fb5376bbf272
s2rgi0 <tlakolu@gmail.com>
2020-12-06 12:59:57 -0600

    added map impl for Visit closes #46

---
+++ pet-clinic-data/src/main/java/xerfio/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package xerfio.springframework.sfgpetclinic.services;
+
+import xerfio.springframework.sfgpetclinic.CrudService;
+import xerfio.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
